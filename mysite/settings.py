c3d23470dbf925e669114ae3672a1a634338dc2e
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2022-10-05 16:55:55 -0600

    Deleted .env. added .env to .gitignore. Changed settings for default DB to be sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#9w)#hfm!#orhwdl#b4iwx3888&&z(o^w=1czo6f-xulqyjt=z'
+# SECRET_KEY = 'django-insecure-#9w)#hfm!#orhwdl#b4iwx3888&&z(o^w=1czo6f-xulqyjt=z'
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,24 +77,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'AIb1SU60FiiBESFO8IbA',
+#         'HOST': 'containers-us-west-57.railway.app',
+#         'PORT': '7471',
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'AIb1SU60FiiBESFO8IbA',
-        'HOST': 'containers-us-west-57.railway.app',
-        'PORT': '7471',
-    }
-}
+# DATABASES = os.getenv('DATABASES')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
