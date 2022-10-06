dead50d06b545f545ed3942df4713e259187fb9d
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2022-10-05 20:52:58 -0600

    Testing migrating database.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question, Choice
+from .models import Question, Choice, Person
 
 # admin.site.register(Question)
 
@@ -19,3 +19,5 @@ class QuestionAdmin(admin.ModelAdmin):
 
 admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Choice)
+
+admin.site.register(Person)
